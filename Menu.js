66775b7ef4b66635bcec66d1b11e73c617af70eb
Ashwini Hegde <sirsiashwini@gmail.com>
2021-04-18 12:43:14 +0530

    Menu fix

--- Menu.js
+++ Menu.js
@@ -1,15 +1,12 @@
 function toggleSubItems(el){
     let subMenu = el.parentElement.lastElementChild;
-    if(subMenu.style.display === 'block'){
-        subMenu.style.display = 'none';
-    }else{
-        subMenu.style.display = 'block';
-    }
+    subMenu.classList.toggle('submenu-open');
+       
     let navMenu = document.getElementsByClassName('nav-menu')[0];
     for(let i=0; i<navMenu.children.length; i++){
         if(navMenu.children[i].firstElementChild !== el){
             let subMenuToClose = navMenu.children[i].lastElementChild;
-            subMenuToClose.style.display = 'none';
+            subMenuToClose.classList.remove('submenu-open')
         }
     }
 }
